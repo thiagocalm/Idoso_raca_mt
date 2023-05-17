90f3e106ef106ac0c65a7623023f822f4454eaef
Thiago Almeida <64435583+thiagocalm@users.noreply.github.com>
2023-05-17 12:15:04 -0300

    Including composition analysis for older adults

--- Analises/scripts/02_descritivas_composicao_por_ciclo_vida.R
+++ Analises/scripts/02_descritivas_composicao_por_ciclo_vida.R
@@ -18,7 +18,16 @@ pnad_plan <- pnad |>
   srvyr::as_survey_design(ids = id_pes,
                           weights = peso)
 
-# Tabela agregada ---------------------------------------------------------
+pnad_idoso_plan <- pnad |>
+  filter(flag_idoso == "Idoso") |>
+  filter(cor_raca != "Outros") |>
+  filter(idade_simples >= 10) |>
+  srvyr::as_survey_design(ids = id_pes,
+                          weights = peso)
+
+rm(pnad)
+
+# 1. Tabela agregada idosos e não idosos ------------------------------------
 
 # Total
 
@@ -127,23 +136,6 @@ t_tem_dep <- pnad_plan |>
       mutate(flag_idoso = "Total") |>
       select(ano, flag_idoso, everything())
   )
-  # mutate(tem_dependente = as.factor(case_when(
-  #   tem_crianca == 1 & tem_idoso_dependente == 0 ~ "Somente criança",
-  #   tem_crianca == 0 & tem_idoso_dependente == 1 ~ "Somente idoso",
-  #   tem_crianca == 1 & tem_idoso_dependente == 1 ~ "Criança e idoso",
-  #   tem_crianca == 0 & tem_idoso_dependente == 0 ~ "Nenhum"))) |>
-  # select(-c(tem_crianca, tem_idoso_dependente)) |>
-  # ungroup() |>
-  # group_by(ano, cor_raca, tem_dependente) |>
-  # summarise(n = n,
-  #           prop_se = prop_se) |>
-  # summarise(ano = ano,
-  #           flag_idoso = flag_idoso,
-  #           cor_raca = cor_raca,
-  #           tem_dependente = tem_dependente,
-  #           n = n,
-  #           prop = round(n/sum(n)*100,2),
-  #           prop_se = prop_se)
 
 # Status de Aposentadoria
 
@@ -163,9 +155,60 @@ t_aposentadoria <- pnad_plan |>
       select(ano, flag_idoso, everything())
   )
 
+# 2. Tabela agregada somente idosos por part. -------------------------------
+
+# Total
+
+t_part_total <- pnad_idoso_plan |>
+  group_by(ano, cor_raca, flag_participa) |>
+  summarise(n = survey_total(),
+            prop = round(survey_mean(na.rm = TRUE)*100,2))
+
+# Sexo
+
+t_part_sexo <- pnad_idoso_plan |>
+  group_by(ano, cor_raca, flag_participa, sexo) |>
+  summarise(n = survey_total(),
+            prop = round(survey_mean(na.rm = TRUE)*100,2))
+
+# Escolaridade
+
+t_part_escolaridade <- pnad_idoso_plan |>
+  group_by(ano, cor_raca, flag_participa, educ_atingida) |>
+  summarise(n = survey_total(),
+            prop = round(survey_mean(na.rm = TRUE)*100,2))
+
+# Quintil de renda
+
+t_part_renda <- pnad_idoso_plan |>
+  group_by(ano, cor_raca, flag_participa, quintil_inc) |>
+  summarise(n = survey_total(),
+            prop = round(survey_mean(na.rm = TRUE)*100,2))
+
+# Tipo de domicilio
+
+t_part_tipodom <- pnad_idoso_plan |>
+  group_by(ano, cor_raca, flag_participa, tipo_dom) |>
+  summarise(n = survey_total(),
+            prop = round(survey_mean(na.rm = TRUE)*100,2))
+
+# Existencia de dependente
+
+t_part_tem_dep <- pnad_plan |>
+  group_by(ano, cor_raca, flag_participa, tem_crianca, tem_idoso_dependente) |>
+  summarise(n = survey_total(),
+            prop = round(survey_mean(na.rm = TRUE)*100,2))
+
+# Status de Aposentadoria
+
+t_part_aposentadoria <- pnad_idoso_plan |>
+  group_by(ano, cor_raca,  flag_participa, flag_aposentadoria) |>
+  summarise(n = survey_total(),
+            prop = round(survey_mean(na.rm = TRUE)*100,2))
 
 # Agrupando resultados ----------------------------------------------------
 
+# 1. Composição entre grupo de idosos e não idosos
 t_agregada <- t_total |>
   mutate(categoria = "Pop. total",
          variaveis = "Total") |>
@@ -237,7 +280,86 @@ t_agregada <- t_total |>
   select(ano, categoria, flag_idoso, cor_raca, variaveis, prop) |>
   arrange(categoria, flag_idoso, cor_raca)
 
+# 2. Composição entre grupo de idosos que participam e nao participam
+
+t_agregada_participa <- t_part_total |>
+  mutate(categoria = "Pop. total",
+         variaveis = "Total") |>
+  select(ano, cor_raca, flag_participa, categoria, variaveis, prop) |>
+  bind_rows(
+    t_part_sexo |>
+      mutate(categoria = "Sexo",
+             variaveis = sexo) |>
+      select(ano, cor_raca, flag_participa, categoria, variaveis, prop)
+  ) |>
+  bind_rows(
+    t_part_escolaridade |>
+      mutate(categoria = "Escolaridade",
+             variaveis = educ_atingida) |>
+      select(ano, cor_raca, flag_participa, categoria, variaveis, prop)
+  ) |>
+  bind_rows(
+    t_part_renda |>
+      mutate(quintil_inc = factor(
+        quintil_inc,
+        levels = c(1,2,3,4,5),
+        labels = c("P20","P40","P60","P80","P100"))) |>
+      mutate(categoria = "Dist. Renda",
+             variaveis = quintil_inc) |>
+      filter(!is.na(variaveis)) |>
+      select(ano, cor_raca, flag_participa, categoria, variaveis, prop)
+  ) |>
+  bind_rows(
+    t_part_tipodom |>
+      mutate(tipo_dom = as.factor(case_when(
+        tipo_dom == 1 ~ "Unipessoal",
+        tipo_dom == 2 ~ "Nuclear",
+        tipo_dom == 3 ~ "Estendida",
+        tipo_dom == 4 ~ "Composta"))) |>
+      mutate(categoria = "Tipo de domicílio",
+             variaveis = tipo_dom) |>
+      filter(!is.na(variaveis)) |>
+      select(ano, cor_raca, flag_participa, categoria, variaveis, prop)
+  ) |>
+  bind_rows(
+    t_part_tem_dep |>
+      mutate(tem_dependente = as.factor(case_when(
+        tem_crianca == 1 & tem_idoso_dependente == 0 ~ "Somente criança",
+        tem_crianca == 0 & tem_idoso_dependente == 1 ~ "Somente idoso",
+        tem_crianca == 1 & tem_idoso_dependente == 1 ~ "Criança e idoso",
+        tem_crianca == 0 & tem_idoso_dependente == 0 ~ "Nenhum"))) |>
+      select(-c(tem_crianca, tem_idoso_dependente)) |>
+      ungroup() |>
+      group_by(ano, cor_raca, flag_participa, tem_dependente) |>
+      summarise(n = n) |>
+      summarise(ano = ano,
+                cor_raca = cor_raca,
+                flag_participa = flag_participa,
+                tem_dependente = tem_dependente,
+                n = n,
+                prop = round(n/sum(n)*100,2)) |>
+      mutate(categoria = "Existência de dependente",
+             variaveis = tem_dependente) |>
+      filter(!is.na(variaveis)) |>
+      select(ano, cor_raca, flag_participa, categoria, variaveis, prop)
+  ) |>
+  bind_rows(
+    t_part_aposentadoria |>
+      mutate(categoria = "Condição de aposentadoria",
+             variaveis = flag_aposentadoria) |>
+      filter(!is.na(variaveis)) |>
+      select(ano, cor_raca, flag_participa, categoria, variaveis, prop)
+  ) |>
+  mutate(flag_participa = case_when(
+    flag_participa == "forca_trabalho" ~ "Na FT",
+    TRUE ~ "Fora da FT"
+  )) |>
+  select(ano, categoria, cor_raca, flag_participa, variaveis, prop) |>
+  arrange(ano, categoria, cor_raca)
+
 # Salvando resultados -----------------------------------------------------
 
 save(t_agregada, file = "./Analises/outputs/pt1/descritivas_composicao_agregado.RData")
+
+save(t_agregada_participa, file = "./Analises/outputs/pt1/descritivas_composicao_agregado_participa_mt.RData")
 
--- Analises/scripts/02_graficos_composicao_agregado.R
+++ Analises/scripts/02_graficos_composicao_agregado.R
@@ -12,6 +12,8 @@ ifelse(!require(patchwork),install.packages("patchwork"),require(patchwork))
 
 load("./Analises/outputs/pt1/descritivas_composicao_agregado.RData")
 
+load("./Analises/outputs/pt1/descritivas_composicao_agregado_participa_mt.RData")
+
 
 # Graficos ----------------------------------------------------------------
 
@@ -75,7 +77,8 @@ graf_df |>
       labels = c("Pop. total","Sexo - Feminino","Sexo - Masculino",
                  "Escolaridade - Nenhum", "Escolaridade - Fund.",
                  "Escolaridade - Médio", "Escolaridade - Superior",
-                 "P20", "P40", "P60", "P80","P100", "Aposentado",
+                 "Dist. renda - P20", "Dist. renda - P40", "Dist. renda - P60",
+                 "Dist. renda - P80","Dist. renda - P100", "Aposentado",
                  "Tem dependente - Criança", "Tem dependente - Idoso",
                  "Tipo dom. - Unipessoal", "Tipo dom. - Nuclear",
                  "Tipo dom. - Estendido")),
@@ -84,6 +87,7 @@ graf_df |>
       levels = c("Nao_idoso", "Idoso"),
       labels = c("Não idoso", "Idoso")
     )) |>
+  filter(variaveis != "Pop. total") |>
   ggplot() +
   aes(x = ano, y = fct_rev(variaveis), fill = prop_fct) +
   geom_tile(color = "white", linewidth = 1.5, linetype = 1) +
@@ -101,6 +105,94 @@ graf_df |>
     legend.text = element_text(size = 9),
     axis.text.x = element_text(angle = 90, size = 8),
     axis.text.y = element_text(size = 9),
-    axis.title = element_text(size = 10, face = "bold", hjust = 1)
+    axis.title = element_text(size = 10, face = "bold", hjust = 1),
+    strip.text = element_text(size = 10, face = "bold")
   )
 
+
+# Grafico para pop. idosa -------------------------------------------------
+
+graf_df_idoso <- t_agregada_participa |>
+  filter(ano %in% c(2012,2015,2019)) |>
+  mutate(ano = as.factor(ano),
+         prop_fct = case_when(
+           prop <= 11 ~ "0-10",
+           prop <= 21 ~ "11-20",
+           prop <= 31 ~ "21-30",
+           prop <= 41 ~ "31-40",
+           prop <= 51 ~ "41-50",
+           prop <= 61 ~ "51-60",
+           prop <= 71 ~ "61-70",
+           prop <= 81 ~ "71-80",
+           prop <= 91 ~ "81-90",
+           prop <= 100 ~ "91-100",
+           TRUE ~ ano),
+         prop_fct = factor(
+           prop_fct,
+           level = c("0-10","11-20","21-30","31-40", "41-50", "51-60", "61-70",
+                     "71-80", "81-90", "91-100"),
+           labels = c("0-10","11-20","21-30","31-40", "41-50", "51-60", "61-70",
+                      "71-80", "81-90", "91-100"))) |>
+  mutate(
+    variaveis = case_when(
+      variaveis == "Sem instrução" ~ "Escolaridade - Nenhum",
+      variaveis == "Ensino Fundamental" ~ "Escolaridade - Fund.",
+      variaveis == "Ensino Médio" ~ "Escolaridade - Médio",
+      variaveis == "Ensino Superior" ~ "Escolaridade - Superior",
+      variaveis == "Somente idoso" ~ "Tem dependente - Idoso",
+      variaveis == "Somente criança" ~ "Tem dependente - Criança",
+      variaveis == "Total" ~ "Pop. total",
+      variaveis == "Feminino" ~ "Sexo - Feminino",
+      variaveis == "Masculino" ~ "Sexo - Masculino",
+      variaveis == "Unipessoal" ~ "Tipo dom. - Unipessoal",
+      variaveis == "Nuclear" ~ "Tipo dom. - Nuclear",
+      variaveis == "Estendida" ~ "Tipo dom. - Estendido",
+      TRUE ~ variaveis)) |>
+  filter(variaveis != "Não aposentado") |>
+  filter(variaveis !=  "Nenhum") |>
+  filter(variaveis != "Estendida") |>
+  filter(variaveis != "Criança e idoso") |>
+  filter(variaveis != "Composta")
+
+# grafico
+
+graf_df_idoso |>
+  mutate(
+    variaveis = factor(
+      variaveis,
+      levels = c("Pop. total","Sexo - Feminino","Sexo - Masculino",
+                 "Escolaridade - Nenhum", "Escolaridade - Fund.",
+                 "Escolaridade - Médio", "Escolaridade - Superior",
+                 "P20", "P40", "P60", "P80","P100", "Aposentado",
+                 "Tem dependente - Criança", "Tem dependente - Idoso",
+                 "Tipo dom. - Unipessoal", "Tipo dom. - Nuclear",
+                 "Tipo dom. - Estendido"),
+      labels = c("Pop. total","Sexo - Feminino","Sexo - Masculino",
+                 "Escolaridade - Nenhum", "Escolaridade - Fund.",
+                 "Escolaridade - Médio", "Escolaridade - Superior",
+                 "Dist. renda - P20", "Dist. renda - P40", "Dist. renda - P60",
+                 "Dist. renda - P80","Dist. renda - P100", "Aposentado",
+                 "Tem dependente - Criança", "Tem dependente - Idoso",
+                 "Tipo dom. - Unipessoal", "Tipo dom. - Nuclear",
+                 "Tipo dom. - Estendido"))) |>
+  filter(variaveis != "Pop. total") |>
+  ggplot() +
+  aes(x = ano, y = fct_rev(variaveis), fill = prop_fct) +
+  geom_tile(color = "white", linewidth = 1.5, linetype = 1) +
+  coord_fixed() +
+  scale_fill_brewer(palette = "RdBu", direction = -1) +
+  theme_minimal() +
+  labs(
+    fill = "Prop (%)",
+    y = "Características",
+    x = "Ano calendário"
+  ) +
+  facet_wrap(~cor_raca + flag_participa, nrow = 1, ncol = 4) +
+  theme(
+    legend.title = element_text(size = 10, face = "bold"),
+    legend.text = element_text(size = 9),
+    axis.text.x = element_text(angle = 90, size = 8),
+    axis.text.y = element_text(size = 9),
+    axis.title = element_text(size = 10, face = "bold", hjust = 1),
+    strip.text = element_text(size = 10, face = "bold")
+  )
